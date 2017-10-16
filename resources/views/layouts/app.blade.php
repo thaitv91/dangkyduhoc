dc872cb429956c79f359e93ce70be59ce74995b3
tuanviet <vietnguyen@luxcer.com>
2017-10-16 13:59:01 +0700

    update map

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -463,69 +463,69 @@
 <script type="text/javascript" src="{{url('js/lang.js')}} "></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript">
-    function initialize() {
-        initMap();
-        //initMapUniver();
-    }
+        function initialize() {
+          initMap();
+          initMapUniver();
+        }
     function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            center: {lat: {{ $footer_lat }}, lng: {{ $footer_long }} },
-            gestureHandling: 'greedy'
+          zoom: 13,
+          center: {lat: {{ $footer_lat }}, lng: {{ $footer_long }} },
+          gestureHandling: 'greedy'
         });
 
         setMarkers(map);      }
 
-    var stations = [];
+     var stations = [];
 
-            @foreach ($footer_location as $location)
-    var temp = [];
-    temp.push("{{ $location[0] }}");
-    temp.push({{$location[1]}});
-    temp.push({{$location[2]}});
-    temp.push({{$location[3]}});
-    stations.push(temp);
-    @endforeach
+      @foreach ($footer_location as $location)
+          var temp = [];
+          temp.push("{{ $location[0] }}");
+          temp.push({{$location[1]}});
+          temp.push({{$location[2]}});
+          temp.push({{$location[3]}});
+          stations.push(temp);
+      @endforeach
+         
 
-    // console.log(beaches);
-
-    function setMarkers(map) {
+      function setMarkers(map) {
         var image = {
-            url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            size: new google.maps.Size(20, 32),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(0, 32)
+          url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          size: new google.maps.Size(20, 32),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(0, 32)
         };
         for (var i = 0; i < stations.length; i++) {
-            var station = stations[i];
-            var features = [
-                {
-                    position: new google.maps.LatLng(station[1], station[2]),
-                },
-            ];
-            features.forEach(function(feature) {
-                var marker = new google.maps.Marker({
-                    position: feature.position,
-                    icon: image,
-                    map: map,
-                });
-                var infowindow = new google.maps.InfoWindow({
-                    content: station[0],
-                });
-                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    return function() {
-                        infowindow.setContent(stations[i][0]);
-                        infowindow.open(map, marker);
-                    }
-                })(marker, i));
-            });
+          var station = stations[i];
+        var features = [
+          {
+            position: new google.maps.LatLng(station[1], station[2]), 
+          },
+          ];
+          features.forEach(function(feature) {
+          var marker = new google.maps.Marker({
+            position: feature.position,
+            icon: image,
+            map: map,
+          });
+          var infowindow = new google.maps.InfoWindow({
+            content: station[0],
+          });
+         google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                  infowindow.setContent(stations[i][0]);
+                  infowindow.open(map, marker);
+                }
+              })(marker, i));
+        });
         }
 
-    }
-</script>
-<script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0mK4AeH9grAllE4tl8Mm6IbM_yPIF0lg&callback=initialize">
-</script>
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0mK4AeH9grAllE4tl8Mm6IbM_yPIF0lg&libraries=places&callback=initialize">
+    </script>
+    @yield('scripts')
     @yield('scripts')
 </body>
 </html>
