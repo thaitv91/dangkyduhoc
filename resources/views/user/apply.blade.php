18ddc964d03ccd96aed58299266c83093fd0faeb
duyenpt <duyenphan@luxcer.com>
2017-10-26 16:07:14 +0700

    add apply success

--- resources/views/user/apply.blade.php
+++ resources/views/user/apply.blade.php
@@ -94,13 +94,13 @@
 				    		<td class="submitted hidden-xs">
 				            <div class="course-status">
 				                    <div class="status">
-				                        <span class="ir app yet tip" title="" data-toggle="tooltip" data-original-title="This course has been shortlisted but not yet submitted to us. Chat with us online if you require some guidance."></span>
+				                        <span class="ir app done tip" title="" data-toggle="tooltip" data-original-title="This course has been shortlisted but not yet submitted to us. Chat with us online if you require some guidance."></span>
 				                    </div>
 				                <div class="status">
 				                    <span class="ir app yet tip" title="" data-toggle="tooltip" data-original-title="No updates has been provided at this stage yet."></span>
 				                </div>
 				                <div class="status">
-				                    <span class="ir app yet tip" title="" data-toggle="tooltip" data-original-title="No updates has been provided at this stage yet."></span>
+				                    <span class="ir app problem tip" title="" data-toggle="tooltip" data-original-title="No updates has been provided at this stage yet."></span>
 				                </div>
 				                <div class="status">
 				                    <span class="ir app yet tip" title="" data-toggle="tooltip" data-original-title="No updates has been provided at this stage yet."></span>
@@ -654,6 +654,219 @@
 				</div>
 			</div>
 		</div><!-- /.continue-to-apply -->
+
+		<div class="apply-success">
+			<div class="row rowsec bgr-white">
+	        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	            <div class="section header">
+	                <h3 class="header-text">CONFIRMATION</h3>
+	            </div>
+	            <div class="section">
+	                <p style="font-size:16px;">Thanks for submitting your university application online. Below is a summary of your application. A copy of this summary has also been emailed to you.</p>
+	            </div>
+	            <div class="section header">
+	                <h3 class="header-text">WHAT'S NEXT?</h3>
+	            </div>
+	            <div class="section">
+	                <p style="font-size:15px;">1) An education consultant from theRightU will be in contact with you within 1-2 working days to confirm that your details and supporting documents are in order.</p>
+	                <p style ="font-size:15px;">2) You WILL be able to make amendments to your choice of courses or upload missing documents and resubmit before your application is processed.</p>
+	                <p style="font-size:15px;">3) If you wish to cancel your application, please drop us a line at <a href="mailto:hello@therightu.com">hello@therightu.com</a></p>
+	            </div>
+	            <div class="section header">
+	                <h3 class="header-text">SUMMARY</h3>
+	            </div>
+	            <div class="section block">
+	                REFERENCE ID: <b>5576</b><br />
+	                DATE SUBMITTED: <b>26/10/2017 4:59:59 PM</b><br />
+	                APPLICATION FEE: <b>WAIVED</b><br />
+	            </div>
+	        </div>
+        
+		    <div class="col-lg-12">
+		            <div class="section header">
+		                <br />
+		                <h3 class="header-text">COURSES APPLIED</h3>
+		            </div>
+		                <div class="section block">
+		                            <div class="row">
+		                                <div class="col-lg-9 clearfix">
+		                                    <span class="name">1. Accounting and Finance</span> <span class="code">(N420)</span> -
+		                                        <span class="university">Robert Gordon University (United Kingdom)</span>
+		                                </div>
+		                            </div>
+		                            <div class="row">
+		                                <div class="col-lg-9 clearfix">
+		                                    <span class="name">2. Bachelor of International Business in Hotel and Tourism Management</span> <span class="code">(CR-001)</span> -
+		                                        <span class="university">Cesar Ritz (Switzerland)</span>
+		                                </div>
+		                            </div>
+		                            <div class="row">
+		                                <div class="col-lg-9 clearfix">
+		                                    <span class="name">3. Accounting and Finance</span> <span class="code">(N420)</span> -
+		                                        <span class="university">Middlesex University (United Kingdom)</span>
+		                                </div>
+		                            </div>
+
+
+		                </div>
+
+
+		        <div id="step_1_header" class="section header">
+		            <br />
+		            <h3 class="header-text">PERSONAL DETAILS</h3>
+		        </div>
+		        <div id="step_1" class="questionaire section block">
+		            <table class="answers_table">
+		                <tr>
+		                    <td>Given name:</td>
+		                    <td>Duyen</td>
+		                </tr>
+		                <tr>
+		                    <td>Family name:</td>
+		                    <td>Phan</td>
+		                </tr>
+		                <tr>
+		                    <td>Email address:</td>
+		                    <td>duyenphan@luxcer.com</td>
+		                </tr>
+		                <tr>
+		                    <td>Gender:</td>
+		                    <td>Female</td>
+		                </tr>
+		                <tr>
+		                    <td>Date of birth:</td>
+		                    <td>8 October 1980</td>
+		                </tr>
+		                <tr>
+		                    <td>Country of birth:</td>
+		                        <td>Vietnam</td>
+		                </tr>
+		                <tr>
+		                    <td>Nationality:</td>
+		                        <td>Vietnamese</td>
+		                </tr>
+		                <tr>
+		                    <td>Passport no.:</td>
+		                    <td>0976645</td>
+		                </tr>
+		                <tr>
+		                    <td>Correspondence address:</td>
+		                    <td>Ha Noi, Viet Nam</td>
+		                </tr>
+		                <tr>
+		                    <td>Permanent address:</td>
+		                    <td>Ha Noi, Viet Nam</td>
+		                </tr>
+		                <tr>
+		                    <td>Mobile number:</td>
+		                    <td>0987545377</td>
+		                </tr>
+		                <tr>
+		                    <td>Home phone no.:</td>
+		                    <td>+84</td>
+		                </tr>
+		                <tr>
+		                    <td>Enrolment year:</td>
+		                    <td>2017</td>
+		                </tr>
+		                <tr>
+		                    <td>Funding source:</td>
+		                    <td>
+
+		                                                
+		                        <text>Scholarship</text>
+		                    </td>
+		                </tr>
+		                <tr>
+		                    <td>
+		                        Free consultation:
+		                    </td>
+		                    <td>
+		                                Yes
+		                    </td>
+		                </tr>
+
+		                <tr>
+		                    <td>Disability:</td>
+		                    <td>No</td>
+		                </tr>
+		                <tr>
+		                    <td>Criminal record:</td>
+		                    <td>No</td>
+		                </tr>
+		            </table>
+		        </div>
+		        <div id="step_2_header" class="section header">
+		            <br />
+		            <h3 class="header-text">EDUCATION &amp; WORK</h3>
+		        </div>
+		        <div id="step_2" class="questionaire section block">
+		                <p>Education background:</p>
+		                <table id="education" class="exp">
+
+		                        <tbody id="1702">
+		                            <tr>
+		                                <td>
+		                                    17 October 2017 - 27 October 2017: BGV (GCE &#39;A&#39; Levels)
+		                                </td>
+		                            </tr>
+		                        </tbody>
+		                </table>
+		                <br />
+
+		                <p>Work experience:</p>
+		                <table id="work" class="exp">
+		                        <tbody id="work_1103">
+		                            <tr>
+		                                <td>
+		                                    2 October 2017 - 31 October 2017 : BGN (Part-time)
+		                                </td>
+		                            </tr>
+		                            <tr>
+		                                <td>
+		                                    HTML/CSS
+		                                </td>
+		                            </tr>
+		                        </tbody>
+
+		                </table>
+		                <br />
+		                            <p>English proficiency tests:</p>
+		                <table id="english" class="exp">
+		                        <tbody id="prof_225">
+		                            <tr>
+		                                <td>
+		                                    31 October 2017 - Occupational English Test (GHN)
+		                                </td>
+		                            </tr>
+		                        </tbody>
+		                </table>
+		                <br />
+		            Supporting documents:
+		                <ul>
+		                            <li>thanhly.doc</li>
+		                </ul>
+
+		        </div>
+
+		            <div id="step_3_header" class="section header open">
+		                <br />
+		                <h3 class="header-text">ADDITIONAL DETAILS</h3>
+		            </div>
+		            <div id="step_3" class="questionaire section block">
+
+		                            </div>
+		            </div>
+
+
+		    </div>
+
+		    <div class="btn-bottom-apply-success">
+				<button class="btn btn-blue">Print</button><br>
+				<button class="btn btn-blue">Exit</button>
+			</div>
+
+		</div><!-- /.apply-success -->
 	</div><!-- /.courses-apply -->
 
 	<div class="fine-print">
